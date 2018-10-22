09efbe9c38335279d4533a0ba00005c4099dffa3
veratrum <veratrum@users.noreply.github.com>
2018-10-23 06:25:58 +1300

    fixed progress graph axis

--- src/namesayer/controller/ProgressScreenController.java
+++ src/namesayer/controller/ProgressScreenController.java
@@ -6,14 +6,17 @@ import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.ValueAxis;
 import javafx.scene.chart.XYChart;
 
 
 public class ProgressScreenController extends CustomController {
 
 	@FXML
-	LineChart<String, Integer> progressGraph;
+	LineChart<Integer, Integer> progressGraph;
 	
 
 	private boolean progressIsUpdated = false;
@@ -21,6 +24,22 @@ public class ProgressScreenController extends CustomController {
 	/**
 	 * A method which is called when the return to main screen button is clicked - returns the user to the main screen
 	 */
+	
+	@Override
+	public void init() {
+		ValueAxis<Integer> xAxis = (ValueAxis<Integer>) progressGraph.getXAxis();
+		ValueAxis<Integer> yAxis = (ValueAxis<Integer>) progressGraph.getYAxis();
+
+		xAxis.setAutoRanging(false);
+		xAxis.setLowerBound(0);
+		xAxis.setUpperBound(10);
+		xAxis.setTickLength(1);
+		
+		yAxis.setAutoRanging(false);
+		yAxis.setLowerBound(0);
+		yAxis.setUpperBound(10);
+		yAxis.setTickLength(1);
+	}
 	
 	@Override
 	public void load() {
@@ -43,13 +62,13 @@ public class ProgressScreenController extends CustomController {
 			protected Void call() throws Exception {
 
 				// Creating the dataSeries to populate the bar Chart
-				XYChart.Series<String, Integer> dataSeries = new XYChart.Series<String, Integer>();
+				XYChart.Series<Integer, Integer> dataSeries = new XYChart.Series<Integer, Integer>();
 				dataSeries.setName("User Performance");
 
 
 				// we need to get the data to populate the bar graph
 				for (int i = 0; i < allUserRatings.size(); i++) {
-					dataSeries.getData().add(new XYChart.Data<String, Integer>(Integer.toString(i), Integer.parseInt(allUserRatings.get(i))));
+					dataSeries.getData().add(new XYChart.Data<Integer, Integer>(i, Integer.parseInt(allUserRatings.get(i))));
 				}
 
 
